a5d46f958e4aa769ce9e1c645f729f60a9a8cb47
AndresPenuela <ap18525@bristol.ac.uk>
2019-12-19 10:27:34 +0000

    Minor corrections

--- B - Implementation/1 - Seasonal weather forecast/Submodules/Read_data.py
+++ B - Implementation/1 - Seasonal weather forecast/Submodules/Read_data.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from netCDF4 import Dataset
 
-def read_csv_data(folder_path,file_name,weather_variable):
+def read_csv_data(folder_path,file_name,column_name = None):
     data = pd.read_csv(folder_path+"//"+file_name)
     ### Dates ###
     # the next day at 00:00 we need to substract one day (-24h) 
@@ -18,11 +18,14 @@ def read_csv_data(folder_path,file_name,weather_variable):
                            format = '%d/%m/%Y')
 
     # Each element of args is the name of weather variable
-    outputs = np.array(data[weather_variable])
+    if isinstance(column_name,(str)):
+        outputs = np.array(data[column_name])
+    else:
+        outputs = np.array(data[data.columns[1:]])
         
     return dates,outputs
 
-def read_netcdf_data(folder_path,file_name,weather_variable):
+def read_netcdf_data(folder_path,file_name,variable_name):
     data = Dataset(folder_path+"//"+file_name, "r")
     ### Dates ###
     # the next day at 00:00 we need to substract one day (-24h) 
@@ -33,7 +36,7 @@ def read_netcdf_data(folder_path,file_name,weather_variable):
                             origin = pd.Timestamp('01-01-1900'))
 
     # Each element of args is the name of weather variable
-    outputs = np.array(data.variables[weather_variable][:])
+    outputs = np.array(data.variables[variable_name][:])
         
     return dates,outputs
         
