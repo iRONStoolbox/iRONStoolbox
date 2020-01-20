a5405f02e3a6793b3a5c6f56131f104a028261a9
AndresPenuela <ap18525@bristol.ac.uk>
2020-01-20 16:37:02 +0000

    Mass balance function corrected
    Qreg_rel[t] = np.array([Qreg_rel[t], s[t] - s_min + I[t] + Qreg_inf[t] - E[t] - env[t]]).max()
    
    was replaced by
    
    Qreg_rel[t] = np.array([Qreg_rel[t], s[t] - s_min + I[t] + Qreg_inf[t] - E[t] - env[t]]).min()

--- Toolbox/Reservoir_system_simulation/Res_sys_sim.py
+++ Toolbox/Reservoir_system_simulation/Res_sys_sim.py
@@ -9,7 +9,12 @@ from numba import njit
 
 # Mass balance function
 @njit(parallel = False) # Set "nopython" mode for best performance, equivalent to @njit
-def Mass_bal_func(I, e, s_0, s_min, s_max, env_min, Qreg_inf, Qreg_rel, s_frac, Policy_inf, Policy_rel):
+def Mass_bal_func(I, e, 
+                  s_0, s_min, s_max, 
+                  env_min, 
+                  Qreg_inf, Qreg_rel, 
+                  s_frac, 
+                  Policy_inf, Policy_rel):
     
     T = I.shape[0]
     ### Declare output variables ###
@@ -56,9 +61,8 @@ def Mass_bal_func(I, e, s_0, s_min, s_max, env_min, Qreg_inf, Qreg_rel, s_frac, 
         else:
             env[t] = env_min[t]
         # If the regulated release (Qreg_rel) is higher than the water resource available (s + I - E - Qenv)
-        # then the release is equal to the higher value between 0 and the resource available 
-        # and the lower value between the latter and the pre-defined release (Qreg_rel)
-        Qreg_rel[t] = np.array([Qreg_rel[t], s[t] - s_min + I[t] + Qreg_inf[t] - E[t] - env[t]]).max()
+        # then the release is equal to the lower value between the resource available and the pre-defined release (Qreg_rel)
+        Qreg_rel[t] = np.array([Qreg_rel[t], s[t] - s_min + I[t] + Qreg_inf[t] - E[t] - env[t]]).min()
         # The spillage is equal to the higher value between 0 and the resource available exceeding the reservoir capacity
         spill[t] = np.array([0,s[t] + I[t] + Qreg_inf[t] - Qreg_rel[t] - env[t] - E[t] - s_max]).max()
         # The final storage (initial storage in the next step) is equal to the storage + inflow - outflows
@@ -67,10 +71,10 @@ def Mass_bal_func(I, e, s_0, s_min, s_max, env_min, Qreg_inf, Qreg_rel, s_frac, 
     return env, spill, Qreg_rel, Qreg_inf, s, E
 
 
-def Res_sys_sim(I_nat, e, s_0, s_min, s_max, env_min, d, Qreg):
+def Res_sys_sim(I, e, s_0, s_min, s_max, env_min, d, Qreg):
     
     # Time length
-    T = I_nat.shape[0]
+    T = I.shape[0]
     # Required environmental compensation flow
     env_min = env_min + np.zeros(T)
     # Required demand
@@ -121,11 +125,72 @@ def Res_sys_sim(I_nat, e, s_0, s_min, s_max, env_min, d, Qreg):
             # file name where it is contained and the parameters of the function
             exec('Policy_inf[i] = '+Qreg['inflows']['function']+'('+str(Qreg['inflows']['param'])+','+str(s_frac[i])+')')
 
-    env, spill, Qreg_rel, Qreg_inf, s, E = Mass_bal_func(I_nat, e, 
-                                            s_0, s_min, s_max, 
-                                            env_min, d, 
-                                            Qreg_inf, Qreg_rel, 
-                                            s_frac,
-                                            Policy_inf,Policy_rel)
+    env, spill, Qreg_rel, Qreg_inf, s, E = Mass_bal_func(I, e, 
+                                                         s_0, s_min, s_max, 
+                                                         env_min, 
+                                                         Qreg_inf, Qreg_rel, 
+                                                         s_frac,
+                                                         Policy_inf,Policy_rel)
                 
     return env, spill, Qreg_rel, Qreg_inf, s, E
+
+N = 100000
+I = np.abs(np.random.normal(loc=20,scale=10,size=N))
+e = np.abs(np.random.normal(loc=4,scale=1,size=N))
+d = np.abs(np.random.normal(loc=20,scale=5,size=N))
+u = np.abs(np.random.normal(loc=20,scale=1,size=N))
+# System constraints
+u_min = 2 # ML/week
+u_max = 80 # ML/week
+# Mean long term release
+u_mean = 20 # ML/week
+# Policy function parameters
+u_ref = 20/u_mean
+s_ref_1 = 0.2
+s_ref_2 = 0.8
+
+### Constraints ###
+s_max = 150 #  (ML) Maximum storage (=reservoir capacity)
+s_min = 0 # (ML) Minimum storage (set to zero for now)
+u_max = 60 # (ML/week) Maximum release capacity
+env_min = 2 # (ML/week)   # Environmental compensation flow
+
+### Initial conditions ###
+s_0 = 80 # (ML) # Storage volume at the beginning of the simulation period
+### Policy function constrains ###
+u_0 = u_min/u_mean # release at storage fraction = 0
+u_1 = u_max/u_mean # release at storage fraction = 1
+
+### Policy function defining points ###
+x0 = [0,       u_0]
+x1 = [s_ref_1, u_ref]
+x2 = [s_ref_2, u_ref]
+x3 = [1,       u_1]
+
+param = [x0, x1, x2, x3, u_mean]
+def four_points_policy(param,*args):
+    x0, x1, x2, x3, u_mean = param
+    si = np.array([x0[0],
+                   np.min([x1[0], x3[0]]),
+                   np.min([x2[0], x3[0]]),
+                   x3[0]])
+    ui = np.array([np.min([x0[1], x1[1], x3[1]]),
+                   np.min([x1[1], x3[1]]),
+                   np.min([x2[1], x3[1]]),
+                   x3[1]])
+    if args:
+        s = args
+        u = np.interp(s, si, ui)*u_mean
+    else:
+        s_step = 0.01
+        s = np.arange(0,1+s_step,s_step)
+        u = np.interp(s, si, ui)*u_mean
+    
+    return u
+u_frac = four_points_policy(param)/u_mean
+
+Qreg = {'releases' : {'file_name' : 'Operating_policy_functions',
+                     'function' : 'four_points_policy',
+                     'param': param},
+        'inflows' : [],
+        'rel_inf' : []}
