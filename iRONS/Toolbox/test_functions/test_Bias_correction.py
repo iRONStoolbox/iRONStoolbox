a198fce88637b3d40ca56297aa317950d44a88a6
AndresPenuela <ap18525@bristol.ac.uk>
2020-03-22 13:47:06 +0000

    Update test_Bias_correction.py

--- iRONS/Toolbox/test_functions/test_Bias_correction.py
+++ iRONS/Toolbox/test_functions/test_Bias_correction.py
@@ -7,9 +7,12 @@ import pandas as pd
 import numpy as np
 from numpy.testing import assert_array_equal
 
-#if __name__ == '__main__':
-import sys
-sys.path.append("../../") # Adds higher directory to python modules path.
+if __name__ == '__main__':
+    import sys
+    sys.path.append("..") # Adds higher directory to python modules path.
+
+import os
+print("The Current Python Directory is"+os.getcwd())
 
 from Data_management.Read_data import read_csv_data
 
@@ -18,7 +21,7 @@ from Weather_forecast.Bias_correction import linear_scaling
 
 ### Observed data ###
 # File path
-path_obs_data = 'Notebooks/B - Implementation/Inputs'
+path_obs_data = '../../Notebooks/B - Implementation/Inputs'
 #path_obs_data = ''
 name_obs_file = 'hist_clim_data.csv'
 # Read files
